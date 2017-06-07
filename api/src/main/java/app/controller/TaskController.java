ea4f757d9a03d6eaf9af50d7f2f236ba646c9e50
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-06-07 23:38:50 +0300

    Create TaskController class

---
+++ api/src/main/java/app/controller/TaskController.java
@@ -0,0 +1,37 @@
+package app.controller;
+
+import app.model.Task;
+import app.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class TaskController {
+
+    @Autowired
+    TaskService taskService;
+
+    @RequestMapping(value = "/task/", method = RequestMethod.GET)
+    public ResponseEntity<List<Task>> listAllTask() {
+
+        return new ResponseEntity<List<Task>>(taskService.findAllTask(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/task/add", method = RequestMethod.GET)
+    public ResponseEntity<String> addTask() {
+        Task task = new Task();
+        task.setTitle("Title");
+        task.setDescription("Description");
+        taskService.saveTask(task);
+
+        return new ResponseEntity<String>("Task added", HttpStatus.OK);
+    }
+
+}
