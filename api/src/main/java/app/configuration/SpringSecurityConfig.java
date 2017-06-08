641c6bd2347c8b0e98a6a2fa6655e0974922b2d1
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-06-08 19:24:05 +0300

    Add override configure method

--- api/src/main/java/app/configuration/SpringSecurityConfig.java
+++ api/src/main/java/app/configuration/SpringSecurityConfig.java
@@ -1,7 +1,10 @@
 package app.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -11,5 +14,14 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("User").password("secret").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/**").access("hasRole('USER')");
+
     }
 }
