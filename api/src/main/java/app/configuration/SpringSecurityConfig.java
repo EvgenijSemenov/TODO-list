b814f61a05b54e9afe4d3c1fb3d4041391208fc3
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-06-08 19:23:12 +0300

    Add configure global method

--- api/src/main/java/app/configuration/SpringSecurityConfig.java
+++ api/src/main/java/app/configuration/SpringSecurityConfig.java
@@ -1,4 +1,15 @@
 package app.configuration;
 
-public class SpringSecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("User").password("secret").roles("USER");
+    }
 }
