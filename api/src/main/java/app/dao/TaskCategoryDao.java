5065f611f1a9e03d8631a8e541215a2227e69a71
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-06-11 00:11:04 +0300

    Create TaskCategoryDao interface

---
+++ api/src/main/java/app/dao/TaskCategoryDao.java
@@ -0,0 +1,18 @@
+package app.dao;
+
+import app.model.Task;
+import app.model.TaskCategory;
+
+import java.util.List;
+
+public interface TaskCategoryDao {
+
+    void save(TaskCategory taskCategory);
+
+    List<TaskCategory> findAll();
+
+    TaskCategory findById(int id);
+
+    void deleteById(int id);
+
+}
