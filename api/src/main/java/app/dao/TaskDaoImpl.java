7ae42f2cb19dfaa9f6d3a91e24cbf1741facea93
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-06-11 00:14:31 +0300

    Add findById and deleteTaskById methods in TaskDaoImpl class

--- api/src/main/java/app/dao/TaskDaoImpl.java
+++ api/src/main/java/app/dao/TaskDaoImpl.java
@@ -2,7 +2,9 @@ package app.dao;
 
 import app.model.Task;
 import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -20,6 +22,21 @@ public class TaskDaoImpl extends AbstractDao<Integer, Task> implements TaskDao {
         Criteria criteria = createEntityCriteria();
 
         return (List<Task>) criteria.list();
+    }
+
+    @Override
+    public Task findById(int id) {
+        Criteria criteria = createEntityCriteria();
+        criteria.add(Restrictions.eq("id", id));
+
+        return (Task) criteria.uniqueResult();
+    }
+
+    @Override
+    public void deleteTaskById(int id) {
+        Query query = getSession().createSQLQuery("delete from Task where id = :id");
+        query.setInteger("id", id);
+        query.executeUpdate();
     }
 
 }
