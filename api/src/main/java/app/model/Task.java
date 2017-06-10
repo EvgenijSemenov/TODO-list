684f85d0e1ca444784e68a3b1b0e8a04eb0cd582
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-06-11 00:16:35 +0300

    Remove title field from Task class

--- api/src/main/java/app/model/Task.java
+++ api/src/main/java/app/model/Task.java
@@ -11,9 +11,6 @@ public class Task {
     @Column(name = "id")
     private int id;
 
-    @Column(name = "title")
-    private String title;
-
     @Column(name = "description")
     private String description;
 
