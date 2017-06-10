2249edc6f85ee04c57eb1525e31f1a7c888a97f8
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-06-11 00:25:55 +0300

    Create findTaskById, saveTask, updateTask and deleteTask in TaskController

--- api/src/main/java/app/controller/TaskController.java
+++ api/src/main/java/app/controller/TaskController.java
@@ -1,14 +1,15 @@
 package app.controller;
 
 import app.model.Task;
+import app.model.TaskCategory;
+import app.service.TaskCategoryService;
 import app.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -18,20 +19,41 @@ public class TaskController {
     @Autowired
     TaskService taskService;
 
+    @Autowired
+    TaskCategoryService taskCategoryService;
+
     @RequestMapping(value = "/task", method = RequestMethod.GET)
     public ResponseEntity<List<Task>> listAllTask() {
 
         return new ResponseEntity<List<Task>>(taskService.findAllTask(), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/task/add", method = RequestMethod.GET)
-    public ResponseEntity<String> addTask() {
-        Task task = new Task();
-        task.setTitle("Title");
-        task.setDescription("Description");
+    @RequestMapping(value = "/task/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Task> findTaskById(@PathVariable int id) {
+
+        return new ResponseEntity<Task>(taskService.findById(id), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/task", method = RequestMethod.POST)
+    public ResponseEntity<Task> saveTask(@RequestBody Task task) {
         taskService.saveTask(task);
 
-        return new ResponseEntity<String>("Task added", HttpStatus.OK);
+        return new ResponseEntity<Task>(task, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/task/{id}", method = RequestMethod.POST)
+    public ResponseEntity<Task> updateTask(@RequestBody Task task) {
+        taskService.updateTask(task);
+
+        return new ResponseEntity<Task>(task, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/task/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Task> deleteTask(@PathVariable int id) {
+        Task deleteTask = taskService.findById(id);
+        taskService.deleteTaskById(id);
+
+        return new ResponseEntity<Task>(deleteTask, HttpStatus.OK);
     }
 
 }
