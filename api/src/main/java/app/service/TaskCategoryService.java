1cb5044dcf29cd45083055ed5bc8fddf8f2ff326
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-06-11 00:12:17 +0300

    Create TaskCategoryService interface

---
+++ api/src/main/java/app/service/TaskCategoryService.java
@@ -0,0 +1,19 @@
+package app.service;
+
+import app.model.TaskCategory;
+
+import java.util.List;
+
+public interface TaskCategoryService {
+
+    void save(TaskCategory taskCategory);
+
+    List<TaskCategory> findAll();
+
+    TaskCategory findById(int id);
+
+    void update(TaskCategory taskCategory);
+
+    void deleteById(int id);
+
+}
