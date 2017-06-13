fde4d2444903fd68717130ac86c8abe46e4c1fa6
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-06-13 09:53:40 +0300

    Add app component methods

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TODO-list';
+
+  private title = 'TODO-list';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+  	console.log(this.checkUserLogin())
+  	if (this.checkUserLogin()) {
+  	  this.router.navigate(['/todo-list']);
+  	}
+  }
+
+  logout() {
+  	localStorage.removeItem("currentUser");
+  }
+
+  checkUserLogin(): boolean{
+    if (localStorage.getItem("currentUser") == null) {
+      return false;
+    }
+    return true;
+  }
+
+  userName(): string {
+    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    return currentUser.name;
+  }
+  
 }
