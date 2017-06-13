052606d90272492ba62f9a7091c20cf449df6105
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-06-13 09:44:24 +0300

    Create task model

--- client/src/app/model/task.ts
+++ client/src/app/model/task.ts
@@ -3,7 +3,7 @@ import { TaskCategory } from './task-category';
 export class Task {
   id: number;
   description: string;
-  doneAtDate: number;
-  doneDate: number;
-  category: TaskCategory;
+  doneAtDate: Date;
+  doneDate: Date;
+  taskCategory: TaskCategory;
 }
